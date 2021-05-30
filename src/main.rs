08fb0fc9f91768f3c3dad7e20d8fb2a6baef06cb
Aron Bergman <aron@aronbergman.se>
2021-05-30 12:25:28 +0200

    Create basic clap app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::App;
+use clap::{crate_name, crate_version};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = cli().get_matches();
+}
+
+fn cli() -> App<'static, 'static> {
+    let app = App::new(crate_name!())
+        .version(crate_version!());
+    app
 }
