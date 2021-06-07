48461f6d148c13a02ef75d08895538d84b2a09cb
Aron Bergman <aron@aronbergman.se>
2021-06-07 15:15:50 +0200

    Add functions to add paths for different directories

--- lib/mold.rs
+++ lib/mold.rs
@@ -10,6 +10,22 @@ impl Mold {
             ..Default::default()
         }
     }
+
+    pub fn add_templates_dir<D: Into<PathBuf>>(&mut self, dir: D) {
+        self.paths.templates.push(dir.into());
+    }
+
+    pub fn add_modules_dir<D: Into<PathBuf>>(&mut self, dir: D) {
+        self.paths.modules.push(dir.into());
+    }
+
+    pub fn add_content_dir<D: Into<PathBuf>>(&mut self, dir: D) {
+        self.paths.content.push(dir.into());
+    }
+
+    pub fn add_static_dir<D: Into<PathBuf>>(&mut self, dir: D) {
+        self.paths.statics.push(dir.into());
+    }
 }
 
 impl Default for Mold {
@@ -36,5 +52,50 @@ impl Default for Paths {
             content: Vec::new(),
             statics: Vec::new(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_single_template_dir() {
+        let mut m = Mold::new();
+
+        m.add_templates_dir("test/");
+
+        assert_eq!(1, m.paths.templates.len());
+        assert_eq!(m.paths.templates.get(0), Some(&PathBuf::from("test/")));
+    }
+
+    #[test]
+    fn add_single_modules_dir() {
+        let mut m = Mold::new();
+
+        m.add_modules_dir("test/");
+
+        assert_eq!(1, m.paths.modules.len());
+        assert_eq!(m.paths.modules.get(0), Some(&PathBuf::from("test/")));
+    }
+
+    #[test]
+    fn add_single_content_dir() {
+        let mut m = Mold::new();
+
+        m.add_content_dir("test/");
+
+        assert_eq!(1, m.paths.content.len());
+        assert_eq!(m.paths.content.get(0), Some(&PathBuf::from("test/")));
+    }
+
+    #[test]
+    fn add_single_static_dir() {
+        let mut m = Mold::new();
+
+        m.add_static_dir("test/");
+
+        assert_eq!(1, m.paths.statics.len());
+        assert_eq!(m.paths.statics.get(0), Some(&PathBuf::from("test/")));
     }
 }
